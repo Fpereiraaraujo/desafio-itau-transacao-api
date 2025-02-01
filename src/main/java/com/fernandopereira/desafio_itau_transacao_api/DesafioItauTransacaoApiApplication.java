69f0bc205b939d28fedb75d2a9169ade6f915cda
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2025-02-01 20:12:44 -0300

    firt commit

---
+++ src/main/java/com/fernandopereira/desafio_itau_transacao_api/DesafioItauTransacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.fernandopereira.desafio_itau_transacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauTransacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauTransacaoApiApplication.class, args);
+	}
+
+}
